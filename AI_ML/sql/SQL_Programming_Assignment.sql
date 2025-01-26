8cb5135fad2778337b2c2631098ae2dbc59d1e4f
Anup Kumar <anup2kin@yahoo.co.in>
2025-01-25 21:09:59 -0500

    Update SQL_Programming_Assignment.sql

--- AI_ML/sql/SQL_Programming_Assignment.sql
+++ AI_ML/sql/SQL_Programming_Assignment.sql
@@ -4,13 +4,14 @@
 select *
 from venue
 WHERE venue.venuecity = 'Washington'
-order by venuename asc
+order by venuename asc;
 
 -- 2. Craft an SQL query to identify and rank the top 10 events with the highest
 --	attendance (assume each sold ticket is an attendance count).
 --	Utilize the event table to extract key details, including event names and attendance
 --	figures, presenting them in descending order based on attendance.
-select E.eventname, SUM(S.qtysold) AS attendance
+select E.eventname,
+	SUM(S.qtysold) AS attendance
 from sales S
 INNER JOIN event E ON E.eventid = S.eventid
 GROUP BY E.eventname
@@ -20,9 +21,8 @@ LIMIT 10;
 -- 3. Construct an SQL query to retrieve events that occurred on a specific date say ‘2008-08-21’,
 --	leveraging both the event and date tables.
 --	The query should extract event details, including names and start times, associated with the specified date.
-SELECT E.eventname, E.starttime, D.year,
-	D.month,
-	D.day
+SELECT E.eventname, E.starttime,
+	D.year, D.month, D.day
 FROM event E
 INNER JOIN date D ON D.dateid = E.dateid
 WHERE CAST(EXTRACT(YEAR FROM CAST(E.starttime AS DATE)) AS INTEGER) = D.year
@@ -32,10 +32,11 @@ WHERE CAST(EXTRACT(YEAR FROM CAST(E.starttime AS DATE)) AS INTEGER) = D.year
 SELECT E.eventname, E.starttime, D.caldate
 FROM event E
 INNER JOIN date D ON D.dateid = E.dateid
-WHERE D.caldate = '2008-08-21'
+WHERE D.caldate = '2008-08-21';
   
 -- 4. Identify the seller who has listed the highest number of tickets utilizing the listing table.
-SELECT L.sellerid, SUM(L.numtickets) AS total_tickets_listed
+SELECT L.sellerid, 
+	SUM(L.numtickets) AS total_tickets_listed
 FROM listing L
 GROUP BY L.sellerid
 ORDER BY total_tickets_listed DESC
@@ -43,10 +44,11 @@ LIMIT 1;
 
 -- 5. Create an SQL query to list categories alongside the total number of events in each category.
 --	Utilize the category and event tables to extract and aggregate relevant information.
-SELECT C.catname, COUNT(E.eventid) AS total_num_events
+SELECT C.catname, 
+	COUNT(E.eventid) AS total_num_events
 FROM category C
 LEFT JOIN event E ON E.catid = C.catid
-GROUP BY C.catname
+GROUP BY C.catname;
 
 -- 6. Formulate an SQL query to calculate the total commission earned by all sellers combined for all
 --	events in a specific city, for instance, 'Kansas City’ utilizing the sales, event and venue tables
@@ -55,26 +57,28 @@ SELECT SUM(S.commission) AS total_comission
 FROM sales S
 INNER JOIN event E ON E.eventid = S.eventid
 INNER JOIN venue V ON V.venueid = E.venueid
-WHERE V.venuecity = 'Kansas City'
+WHERE V.venuecity = 'Kansas City';
 
 
-SELECT S.sellerid, V.venuecity, SUM(S.commission) AS total_commision
+SELECT S.sellerid, V.venuecity, 
+	SUM(S.commission) AS total_commision
 FROM sales S
 INNER JOIN event E ON E.eventid = S.eventid
 INNER JOIN venue V ON V.venueid = E.venueid
 GROUP BY S.sellerid, V.venuecity
-ORDER BY S.sellerid
+ORDER BY S.sellerid;
 
 -- 7. Devise an SQL query to calculate the average ticket price for each event category.
 --	Utilizing the category, event, and sales tables, extract relevant information and compute the average
 --	ticket price by considering the ratio of total price paid* to the quantity sold for each event.
 --	* Interpret for each individual row ‘pricepaid’ as total price paid for ‘qtysold’ number of tickets
-SELECT C.catname, AVG(S.pricepaid / S.qtysold) AS avg_ticket_price
+SELECT C.catname, 
+	AVG(S.pricepaid / S.qtysold) AS avg_ticket_price
 FROM category C
 INNER JOIN event E ON E.catid = C.catid
 INNER JOIN sales S ON S.eventid = E.eventid
 WHERE S.qtysold IS NOT NULL OR S.qtysold <> 0
-GROUP BY C.catname
+GROUP BY C.catname;
 
 
 -- 8. Determine the total sales and average commission for each event category by creating an SQL subquery
@@ -82,7 +86,9 @@ GROUP BY C.catname
 --	sales and average commission.
 WITH sales_data_per_cat_id AS
 (
-	SELECT E.catid, COUNT(S.salesid) as total_sales, AVG(S.commission) as avg_comission
+	SELECT E.catid,
+		COUNT(S.salesid) as total_sales,
+		AVG(S.commission) as avg_comission
 	FROM sales S
 	LEFT JOIN event E ON E.eventid = S.eventid
 	GROUP BY E.catid
@@ -91,7 +97,7 @@ WITH sales_data_per_cat_id AS
 SELECT *
 FROM Category C
 LEFT JOIN sales_data_per_cat_id S ON S.catid = C.catid
-WHERE S.catid IS NOT NULL
+WHERE S.catid IS NOT NULL;
 
 -- 9. Generate an SQL query to create a view that displays events with a total revenue exceeding 50,000.
 --	using the sales table and appropriate grouping and conditions.
@@ -114,5 +120,5 @@ SELECT dateid,
 	LAG(SUM(qtysold)) OVER (ORDER BY dateid) AS previous_day_total_sales
 FROM sales
 GROUP BY dateid
-ORDER BY dateid
+ORDER BY dateid;
 
